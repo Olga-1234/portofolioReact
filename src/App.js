6ef19fc06b560cb6d5be2d9d460891970a14c336
Olga Mujinga <monaolgita@gmail.com>
2022-01-21 11:45:05 +0100

    feat: add google analytics with react

--- src/App.js
+++ src/App.js
@@ -6,17 +6,13 @@ import Home from "./Home";
 import Other from "./Skills";
 import Project from "./project";
 import Footer from "./Footer";
-import reactGa from "react-ga";
-import {useEffect} from 'react'
+
 
 // import NavBar from "./Navbar";
 
 function App() {
 
-useEffect(() => {
-reactGa.initialize('G-6QL96YF90R')
-reactGa.pageview("/")
-}, [])
+
     return (
     <div className="col-12">
     
