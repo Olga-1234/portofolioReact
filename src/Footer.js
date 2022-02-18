0c004c30fc2f4a59c41a63521fc8bc4bd3fa6735
Olga Mujinga <monaolgita@gmail.com>
2022-02-18 09:07:24 +0100

    refactor: changement du style au niveau responsive

--- src/Footer.js
+++ src/Footer.js
@@ -6,23 +6,22 @@ const Footer = () => {
     >
       <div className="container">
         <p className="h3 fw-bold mt-5 mb-5">CONTACTS</p>
-        <div className="d-flex  justify-content-between" >
-        <div className="d-flex justify-content-between">
-          <div className="col-12 col-sm-12">
+        <div className="d-flex col-12 justify-content-between" >
+  
+          <div className="col col-sm-4">
             <p className="fw-bold">Téléphone</p>
             <p>+243 82 20 28 987</p>
           </div>
-          <div className="col-12 col-sm-12">
+          <div className="col-12 col-sm-4">
             <p className="fw-bold">Email</p>
             <a href="mailto:monaolgita@gmail.com" className=" border " target="blank">
               monaolgita@gmail.com
             </a>
           </div>
-        </div>
-        <div className="col-4">
+        
+        <div className="col-12 col-sm-4">
          
 
-          <div className=" col">
             <p className="fw-bold h4">Reseaux Sociaux</p>
 
             <a href="https://github.com/Olga-1234 " target="blank">
@@ -39,7 +38,6 @@ const Footer = () => {
             </a>
           </div>
         </div>
-      </div>
       </div>
     </div>
   );
